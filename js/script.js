1aaeaed482579135ffe9e34e70b792299dea29de
jingyiweiofficial-creator <jingyiweiofficial@gmail.com>
2026-05-05 22:59:52 +0800

    v5

--- js/script.js
+++ js/script.js
@@ -23,15 +23,15 @@ document.addEventListener("DOMContentLoaded", function() {
 // ==========================================
 const spiritData =[
     {
-        img: "./img/spirit1.png",
+        img: "./img/spirit.jpg",
         text: "守護著「水井村的鑿井歷史」。我們挖掘在地故事，將那份刻苦耐勞、為生命開拓水源的韌性，轉化為服裝上的符號。"
     },
     {
-        img: "./img/spirit2.png",
+        img: "./img/spirit2.jpg",
         text: "守護著「每一塊布料的生命」。堅持零廢棄原則，將舊衣與剩料解構再造，讓被遺忘的物件重新成為文化的鎧甲。"
     },
     {
-        img: "./img/spirit3.png",
+        img: "./img/spirit3.jpg",
         text: "守護著「在地長輩的職人工藝」。我們培訓水井村的長輩負責縫紉，讓歲月累積的指尖溫度，溫暖每一位穿上井衣衛的人。"
     }
 ];
